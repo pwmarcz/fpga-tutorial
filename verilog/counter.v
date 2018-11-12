dd7ccb600252a3911d2c27fc159876f70ad6d8ef
Paweł Marczewski <pwmarcz@gmail.com>
2018-11-12 16:29:46 +0100

    Remove counter implementation

--- verilog/counter.v
+++ verilog/counter.v
@@ -1,15 +1,6 @@
-module counter(input wire        clk,
-               input wire        en,
-               input wire        rst,
+module counter(input wire       clk,
+               input wire       en,
+               input wire       rst,
                output reg [3:0] count);
-
-  initial count = 0;
-
-  always @(posedge clk) begin
-    if (rst)
-      count <= 0;
-    else if (en)
-      count <= count + 1;
-  end
-
+  // TODO
 endmodule
